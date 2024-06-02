c163ead49de457709c8c7597335d495e9a2a1bd2
mowang <3213744542@qq.com>
2024-06-03 00:27:42 +0800

    pom文件以及项目路径

---
+++ src/main/java/com/bbs/cloud/admin/Main.java
@@ -0,0 +1,12 @@
+package com.bbs.cloud.admin;
+
+
+/**
+ * @Author 李志豪
+ * @Date 2024/6/2 15:36
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
