bb399d121a70ee1d2fd76f5c27275793ee58598f
li1165631719 <li1165631719@users.noreply.github.com>
2024-06-03 10:29:07 +0800

    增加注册中心模块

---
+++ study_basis_admin_server/src/main/java/com/bbs/cloud/admin/server/Application.java
@@ -0,0 +1,17 @@
+package com.bbs.cloud.admin.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author 李志豪
+ * @create 2024/6/3
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
